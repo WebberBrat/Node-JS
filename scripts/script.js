8e3160877d584eb4f1f74114a024bf26524b536e
Vlad Melykh <67951836+WebberBrat@users.noreply.github.com>
2024-11-17 23:30:48 +0200

    Update script.js

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-const carButtons = document.querySelectorAll('.car-button');
-carButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        alert(`Вартість оренди ${carName}: ${carPrices[carName] || 'не встановлена'}`);
-    });
-});
